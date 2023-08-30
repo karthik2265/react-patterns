e3443a77aa06928e501b43c3affe674a093254b4
karthik2265 <63234167+karthik2265@users.noreply.github.com>
2023-08-30 19:10:12 +0530

    add modal layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import "./App.css";
-import HOC from "./patterns/HOC";
+import Modal from "./patterns/Layout/Modal";
 
 function App() {
+  const [showTermsAndConditions, setShowTermsAndConditions] = useState();
   return (
     <>
-      <HOC />
+      <button onClick={() => setShowTermsAndConditions(true)}>Terms and Conditions</button>
+      <Modal isOpen={showTermsAndConditions}>
+        <div>
+          <h1>Terms and Conditions</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta molestias maxime nesciunt neque laboriosam,
+            harum expedita quidem dignissimos, aliquid impedit illo voluptatum? Possimus voluptatem quia modi molestias
+            deleniti, voluptas facere?
+          </p>
+          <button onClick={() => setShowTermsAndConditions(false)}>Close</button>
+        </div>
+      </Modal>
     </>
   );
 }
